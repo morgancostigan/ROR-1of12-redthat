feb6f5ce45ec68c1b88107e4aa7d27c80c92e8cd
morgan costigan <morgan.costigan@gmail.com>
2019-03-29 14:49:32 -0500

    adds authentication, removes edit and destroy links from unauthorized users, removes new link button in favour of submit link for auth users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_190929) do
+ActiveRecord::Schema.define(version: 2019_03_29_191109) do
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_03_26_190929) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
